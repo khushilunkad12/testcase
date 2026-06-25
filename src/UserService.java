75efdaed5d81687bec7ccbbe173cf3745703e526
khushi <lunkadkhushi52@gmail.com>
2026-06-25 18:14:42 +0530

    Add buggy Java project

---
+++ src/UserService.java
@@ -0,0 +1,10 @@
+public class UserService {
+
+    public static void main(String[] args) {
+
+        String name = null;
+
+        System.out.println("User Name Length: " + name.length());
+
+    }
+}
